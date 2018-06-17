819c9d13e09e47c920332f2913d0ae7dcea76478
Rasis0923 <cipher17126@gmail.com>
2018-06-17 20:10:06 +0900

    アイテムの調整＆効果音の追加

--- Hockey/Assets/T/Script/Thunder.cs
+++ Hockey/Assets/T/Script/Thunder.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Thunder : MonoBehaviour {
-
+    Rigidbody2D rb2D;
     // Use this for initialization
     void Start()
     {
-        Rigidbody2D rb2D = GetComponent<Rigidbody2D>();
-        Vector3 vel = rb2D.velocity;
-        vel.y *= -1;
-        rb2D.velocity = vel;
-        Destroy(this.gameObject);
+        Vector2 max = Camera.main.ViewportToWorldPoint(Vector2.one);
+        int SclW = 5;
+        for(int i = 0; i < 4; i++)
+        {
+            transform.GetChild(i).gameObject.GetComponent<ParticleSystem>().startSize = max.y / SclW;
+        }
+        rb2D = transform.parent.GetComponent<Rigidbody2D>();
+        rb2D.velocity = Vector3.zero;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(rb2D.velocity.magnitude > 0.2f)
+        {
+            Destroy(this.gameObject);
+        }
 	}
 }
